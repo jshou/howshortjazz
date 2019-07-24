e847650b3041a0efe1567deee75f66a8b94780fa
jshou <joshuahou@gmail.com>
2019-07-24 08:31:38 -0700

    upgrade build system

--- webpack/base.config.js
+++ webpack/base.config.js
@@ -10,7 +10,7 @@ module.exports = {
       use: [{
         loader: 'babel-loader',
         query: {
-          presets: ['es2015', 'react']
+          presets: ['@babel/preset-env', '@babel/preset-react']
         }
       }]
     }]
