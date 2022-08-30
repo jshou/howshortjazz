d54d29ccc3972184c61a5769255f6bbc04a0e048
jshou <joshuahou@gmail.com>
2022-08-30 13:32:07 -0700

    remove js gig calendar, not booking much anwyay

--- webpack/prod.config.js
+++
@@ -1,7 +0,0 @@
-const webpack = require('webpack');
-const merge = require('webpack-merge');
-const baseConfig = require('./base.config.js');
-
-module.exports = merge(baseConfig, {
-  mode: 'production'
-})
